44b5208c09b34ccbfdec29c2142838c7adcf89ec
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-03-19 15:51:13 +0000

    creats modals

---
+++ app/(modals)/booking.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+function Page() {
+  return (
+    <View>
+      <Text>Bookings</Text>
+    </View>
+  )
+}
+
+export default Page
