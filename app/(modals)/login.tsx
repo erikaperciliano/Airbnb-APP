b4865f9b4013c1680a26ca01d97715c5d6d26805
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-04-13 19:19:34 +0100

    calls the hook

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,10 +1,24 @@
+import { defaultStyles } from '@/constants/Styles';
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser'
 import React from 'react'
-import { Text } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 
 function Page() {
+  useWarmUpBrowser();
+
   return (
-    <Text>Login</Text>
+    <View style={styles.container}>
+      <TextInput autoCapitalize='none' placeholder='Email' style={defaultStyles.inputField}/>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#fdffff',
+    padding: 26
+  }
+})
 
 export default Page
